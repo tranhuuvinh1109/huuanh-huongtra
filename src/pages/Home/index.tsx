23b577647b8703392dc456ff1ebac81e351495a1
tranhuuvinh1109@gmail.com <tranhuuvinh1109@gmail.com>
2025-05-03 09:05:14 +0700

    main: add video auto play

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,7 +10,15 @@ const HomePage = () => {
       <MessageCongratulation />
       <BankAccount />
       <Album />
+
       <div className="h-24"></div>
+      <video width="320" height="240" controls autoPlay>
+        <source
+          src="https://firebasestorage.googleapis.com/v0/b/realtime-cnn.appspot.com/o/video.mp4?alt=media&token=d053a8ad-46c1-4a9d-8f08-846a852f4836"
+          type="video/mp4"
+        />
+        <source src="movie.ogg" type="video/ogg" />
+      </video>
     </div>
   );
 };

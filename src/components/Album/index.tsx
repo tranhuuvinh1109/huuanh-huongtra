4ab0ee17ffbfe5b78af3e6459543e96043f1c9ae
tranhuuvinh1109@gmail.com <tranhuuvinh1109@gmail.com>
2025-05-03 17:03:49 +0700

    update

--- src/components/Album/index.tsx
+++ src/components/Album/index.tsx
@@ -1,6 +1,31 @@
+import { useEffect, useRef } from "react";
 import { imageArray } from "../../constant/common";
 
 const Album = () => {
+  const scrollerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+      addAnimation();
+    }
+  }, []);
+
+  function addAnimation() {
+    const scroller = scrollerRef.current;
+
+    if (scroller) {
+      scroller.setAttribute("data-animated", "true");
+
+      const scrollerInner = scroller.querySelector(".scroller__inner") as HTMLUListElement;
+      const scrollerContent = Array.from(scrollerInner.children) as HTMLElement[];
+
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true) as HTMLElement;
+        duplicatedItem.setAttribute("aria-hidden", "true");
+        scrollerInner.appendChild(duplicatedItem);
+      });
+    }
+  }
   // const [listImage, setListImage] = useState<string[]>([]);
 
   // useEffect(() => {
@@ -22,12 +47,18 @@ const Album = () => {
   // }, []);
 
   return (
-    <div className="mt-10 grid grid-cols-3 gap-4 px-6">
-      {imageArray.map((item, index) => (
-        <div key={index} className="mb-4 w-full overflow-hidden">
-          <img src={item.url} alt={`image-${index}`} className="w-full rounded-md" />
-        </div>
-      ))}
+    <div className="mt-24 flex items-center justify-center	">
+      <div ref={scrollerRef} className="scroller mx-auto my-auto max-w-[1280px]" data-speed="slow">
+        <ul className="tag-list scroller__inner">
+          {imageArray.map((item, index) => {
+            return (
+              <li key={index} className="relative min-h-[250px] w-[250px]">
+                <img src={item.url} alt={`image-${index}`} className="z-10" style={{ objectFit: "cover" }} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
--- src/components/VideoIntro/index.tsx
+++ src/components/VideoIntro/index.tsx
@@ -6,9 +6,7 @@ const VideoIntro = () => {
   useEffect(() => {
     const playVideo = () => {
       if (videoRef.current) {
-        videoRef.current.play().catch((err) => {
-          console.log("Autoplay error:", err);
-        });
+        videoRef.current.muted = false;
       }
     };
 
@@ -23,7 +21,7 @@ const VideoIntro = () => {
 
   return (
     <div className="px-6 py-10">
-      <video ref={videoRef} className="w-full rounded-md" controls autoPlay>
+      <video ref={videoRef} className="w-full rounded-md" controls autoPlay muted>
         <source
           src="https://firebasestorage.googleapis.com/v0/b/realtime-cnn.appspot.com/o/video.mp4?alt=media&token=d053a8ad-46c1-4a9d-8f08-846a852f4836"
           type="video/mp4"

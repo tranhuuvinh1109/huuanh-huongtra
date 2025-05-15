1e56449e1a903410d094b25aa4054723d983f072
tranhuuvinh1109@gmail.com <tranhuuvinh1109@gmail.com>
2025-05-15 21:57:54 +0700

    add images

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,17 @@
 const Footer = () => {
   return (
-    <div className="mt-10 px-6 text-white">
-      <h1 className="text-2xl font-bold ">💌 Lời Cảm Ơn Chân Thành</h1>
-      <p className="mt-6">
-        Chúng tôi xin gửi lời cảm ơn sâu sắc đến quý khách đã dành thời gian quý báu để chia vui cùng gia đình chúng tôi
-        trong ngày trọng đại.
-      </p>
-      <p>Sự hiện diện và lời chúc phúc của quý vị là niềm vinh hạnh và là món quà quý giá nhất đối với chúng tôi.</p>
+    <div className="mt-10 flex justify-center px-6 text-white">
+      <div className="w-2/3 text-center md:w-1/2">
+        <h1 className="text-2xl font-bold ">💌 Lời Cảm Ơn Chân Thành</h1>
+        <p className="mt-6">
+          Chúng tôi xin gửi lời cảm ơn sâu sắc đến quý khách đã dành thời gian quý báu để chia vui cùng gia đình chúng
+          tôi trong ngày trọng đại.
+        </p>
+        <p>Sự hiện diện và lời chúc phúc của quý vị là niềm vinh hạnh và là món quà quý giá nhất đối với chúng tôi.</p>
 
-      <h5>Trân trọng,</h5>
-      <h4 className="text-center text-2xl font-semibold">Hữu Anh & Hương Trà</h4>
+        <h5>Trân trọng,</h5>
+        <h4 className="mt-10 text-center text-2xl font-semibold">Hữu Anh & Hương Trà</h4>
+      </div>
     </div>
   );
 };

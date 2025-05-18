c01a6e24d747f71cd7da0673f5a1bf5131ae00d3
tranhuuvinh1109@gmail.com <tranhuuvinh1109@gmail.com>
2025-05-18 12:05:17 +0700

    update style

--- src/components/RoadMap/index.tsx
+++ src/components/RoadMap/index.tsx
@@ -41,7 +41,7 @@ const RoadMap = () => {
     <>
       <div className="mt-24">
         <h1 className="mb-6 text-center font-playpen text-3xl font-semibold text-grow-custom">Sự Kiện</h1>
-        <div className="my-12 flex w-full justify-start gap-6 overflow-hidden px-6 sm:justify-center">
+        <div className="my-12 flex w-full justify-start gap-6 px-6 sm:justify-center">
           <div className="relative block w-1 bg-grow-custom lg:hidden">
             <div className="absolute  left-[-12px]  top-4  h-6  w-6 translate-x-[2px] rounded-full  bg-grow-custom"></div>
             <div
